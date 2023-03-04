c75a8f0e06e5d4d630a276b52bb644c41e019b53
Guilherme Azevedo <guilhermedagama2021100@gmail.com>
2023-03-04 10:16:52 -0300

    feat: excluindo index.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+PORT = process.env.PORT || 3001
+
 app.get('/', (req, res) => res.status(200).json({ message: 'Olá Mundo!' }));
 
-module.exports = app;
+app.listen(PORT, () => console.log('server running on port 3001'));
--- src/index.js
+++
@@ -1,5 +0,0 @@
-const app = require('./app');
-
-PORT = process.env.PORT || 3001
-
-app.listen(PORT, () => console.log('server running on port 3001'));
